05dc81aa3debebcd085fe20d8cf023b31df53b90
mrDzamba <dzamba@gmail.com>
2018-09-09 01:43:19 +0200

    adding print of array

--- test.c
+++ test.c
@@ -1,10 +1,15 @@
 // this is test file
+#include "stdio.h"
+#include "math.h"
 void int main(int argc, char const *argv[]) {
   /* code */
   int i = 0;
+  char array[] = "OleKazemJa";
   for(i = 0; i < 10; i++){
     printf("i = %d\n", i);
   }
-  
+  for(i = 0; i < strlen(array); i++){
+    printf("i = %d letter = %c\n", i, aray[i]);
+  }
   return 0;
 }
